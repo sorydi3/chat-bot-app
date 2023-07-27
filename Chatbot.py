7c66481716790601e249a574a7839cdf1499966e
Diallo <ibrahimasory22@hotmail.com>
2023-07-27 12:36:45 +0000

    bug fix

--- Chatbot.py
+++ Chatbot.py
@@ -9,12 +9,6 @@ from langchain.utilities import SerpAPIWrapper
 from langchain.callbacks import StdOutCallbackHandler
 from langchain.llms import OpenAI
 import pandas as pd
-
-from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI as PandasOpenAI
-
-
-
 import os
 # CODI_AUDITORIA,dbo_EMPRESAS.ENTITAT,CODI,CODI_SERVEI,Search Name,TIPUS_PROGRAMA,ESTAT_OPORTUNITAT,ESTAT_RESOLUCIO_CONVENI,ANY_CONVOCATORIA,CONVOCATORIA,CODI_PROGRAMA_,NAPR,NOM_ACCIO,CODI_SERVEI_ESPECIALITAT_UP,NUM_EXPEDIENT,ACTIVACIO_PROGRAMES,DATA_INICI_PROGRAMA,DATA_INICI_VERIFICADA,DATA_FI_PROGRAMA,DATA_FINAL_VERIFICADA,
 with st.sidebar:
@@ -27,15 +21,12 @@ with st.sidebar:
     dt.drop(columns_to_drops, inplace=True, axis=1)
     dt.rename(columns={'Search Name': 'NOM_BUSQUEDA'})
     os.environ["OPENAI_API_KEY"] = openai_api_key
-
-    ## save dataframe to session state
-    st.session_state["csv_file_dt"] = dt
     # convert to csv
     csv_file = dt.to_csv('./cursos_.csv', index=False)
     st.write(dt)
 
 
-st.title("🔎 Suport virtual")
+st.title("🔎 Gentis chatbot")
 st.image("./gentis5.png")
 
 # add image to the center of the page using markdown
@@ -50,7 +41,7 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Quins cursos hi ha a Girona?"):
+if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
@@ -58,22 +49,30 @@ if prompt := st.chat_input(placeholder="Quins cursos hi ha a Girona?"):
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    df = st.session_state["csv_file_dt"]
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo",
+                     openai_api_key=openai_api_key, streaming=True)
+    search = DuckDuckGoSearchRun(name="Search")
 
-    llm = PandasOpenAI(api_token=openai_api_key)
+    agent_csv = create_csv_agent(
+        OpenAI(temperature=0),
+        "cursos_.csv",
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
 
-    pandas_ai = PandasAI(llm,conversational=True, verbose=True)
+    tools = load_tools(['ddg-search'])
 
-
-
-    
+    search_agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                                    verbose=True, handle_parsing_errors="CHECK YOUR INPUT", max_iterations=10)
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(
             st.container(), expand_new_thoughts=False)
-        language_prompt = "The response  of the user question must always be in Catalan. \n\n"
-        prompt = prompt + "\n\n" + language_prompt
-        response = pandas_ai(df,prompt=prompt)
-        print("Pandas AI RESPONSE", response)
+        response = agent_csv.run(
+            prompt, callbacks=[st_cb])
         st.session_state.messages.append(
             {"role": "assistant", "content": response})
         st.write(response)
+
+
+
+
